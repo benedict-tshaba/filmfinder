d48200776fd209717d25aa4df439642157f340f9
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2018-11-13 14:59:50 +0200

    Create spinner when app is loading resources

--- src/components/films/Films.js
+++ src/components/films/Films.js
@@ -2,13 +2,30 @@ import React, {Component} from 'react';
 
 import {Consumer} from '../../context';
 
+import Spinner from '../layout/Spinner';
+import Film from '../films/Film';
+
 class Films extends Component {
 	render() {
 		return (
 			<Consumer>
 			  {value => {
+			  	const {film_list, heading} = value;
 			  	console.log(value);
-			  	return <h1>Films</h1>
+			  	if(film_list === undefined || film_list.length === 0) {
+			  		return <Spinner />;
+			  	} else {
+			  		return (
+			  			<React.Fragment>
+			  			  <h3 className="text-center mb-4">{heading}</h3>
+			  			<div className="row">
+			  			  {film_list.map( (item) => (
+			  			  	<Film key={item.imdbID} film={item} />
+			  			  ) )}
+			  			</div>
+			  			</React.Fragment>
+			  		);
+			  	}
 			  }}
 			</Consumer>
 		);
