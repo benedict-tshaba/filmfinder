d6a8c8dffa5f71c9df56c17acbd30b8ab27c0b9f
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2018-11-23 13:54:58 +0200

    Updates

---
+++ precache-manifest.34501371302f3698255f5200de478f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a85f08e633a2465b5af",
+    "url": "/filmfinder/static/js/runtime~main.8a85f08e.js"
+  },
+  {
+    "revision": "1c68ea71099d1fdc11ec",
+    "url": "/filmfinder/static/js/main.1c68ea71.chunk.js"
+  },
+  {
+    "revision": "1c463376c93be5a15737",
+    "url": "/filmfinder/static/js/1.1c463376.chunk.js"
+  },
+  {
+    "revision": "1c68ea71099d1fdc11ec",
+    "url": "/filmfinder/static/css/main.80af1110.chunk.css"
+  },
+  {
+    "revision": "c41e890517c512edcbcb6e617de6b7dd",
+    "url": "/filmfinder/index.html"
+  }
+];
